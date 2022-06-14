956cb4e45461a659a781557cb52b6356f530c0a6
Werick <werick2000@live.com>
2022-06-13 21:03:21 -0300

    Fibonacci

---
+++ TargetSistemasFibonacci/Desafio/Fibonacci.cs
@@ -0,0 +1,28 @@
+﻿    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira um número: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int  i = 0, num1 = 1, num2 = 1;
+             
+            while (i < num)
+            {
+                i = num1 + num2;
+                num2 = num1;
+                num1 = i;
+
+                if(i == num)
+                {
+                    Console.WriteLine("Seu número pertence a sequência Fibonacci!");
+                    break;
+                }
+                else if(i > num)
+                {
+                    Console.WriteLine("Seu Número não pertence a Sequência Fibonacci!");
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
