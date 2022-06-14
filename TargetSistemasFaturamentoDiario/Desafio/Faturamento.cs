75440fb57e0afb948e17c88b16c2624d4e096cf9
Werick <werick2000@live.com>
2022-06-13 21:40:26 -0300

    Desafio concluido

---
+++ TargetSistemasFaturamentoDiario/Desafio/Faturamento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+ 
+ class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var json = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"\Dados.json");
+
+            var dados = JsonConvert.DeserializeObject<List<Dados>>(json);
---
+++ TargetSistemasString/Desafio/String.cs
@@ -0,0 +1,16 @@
+﻿
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite sua frase: ");
+            string StringOriginal = Console.ReadLine();
+            string StringNova = string.Empty;
+            for(int i = (StringOriginal.Length -1 );i >= 0; i--)
+            {
+                StringNova += Convert.ToString(StringOriginal[i]);
+            }
+            Console.WriteLine("String Invertida: " + StringNova);
+            Console.ReadLine();
+        }
+    }
